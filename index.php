a155786aa92b86f069506b3bf717d18e7bac563d
Kavishx <47065068+Kavindu1446@users.noreply.github.com>
2024-05-14 17:44:19 +0530

    index update

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include 'header.inc'; ?>
+<?php include 'menu.inc'; ?>
+
+<main>
+    <h1>Welcome to the Quiz Website</h1>
+    <p>This website allows you to take quizzes and track your performance.</p>
+</main>
+
+<?php include 'footer.inc'; ?>
